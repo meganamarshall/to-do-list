f5672ec8f478a60957bfa5b29941b9a1acb0d912
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-09-04 12:04:15 -0700

    mock data

---
+++ data/list.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    task: 'take out the trash'
+  },
+  {
+    task: 'go to work'
+  },
+  {
+    task: 'do the laundry'
+  },
+  {
+    task: 'get a job'
+  },
+  {
+    task: 'clean my room'
+  }
+]
---
+++ data/user.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+    email: 'megan@test.com',
+    hash: '12lkjlslkjgds',
+    displayName: 'Megan'
+  },
+  {
+    email: 'test@test.com',
+    hash: '3r8o8haer;fih',
+    displayName: 'Bob'
+  }
+  ];
